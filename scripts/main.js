6d22643d21613c6152796ce027314781007112aa
badview08 <jonesgabriel21@gmail.com>
2015-05-28 14:42:39 -0400

    Added basic background docs and scripts

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+(function(){
+  'use strict';
+
+  $(document).ready(function(){
+    $.ajax({
+      url: "https://api.github.com/user",
+      headers: {
+        "Authorization": "token " + GITHUB_TOKEN
+      }
+    }).then(function(user) {
+      console.log(user);
+    });
+  });
+
+  $(document).ready(function(e){
+    var code = window.location.href.match(/\?code=(.*)/)[1];
+    if(code) {
+      $.getJSON('http://localhost:9999/authenticate/'+code, function(data) {
+       console.log(data);
+      });
+    }
+  });
+
+  $('button').on('click', function(e){
+    window.location.replace('https://github.com/login/oauth/authorize?client_id=1932e9016f4a991428bb');
+  });
+})();
